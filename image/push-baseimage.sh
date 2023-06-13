08521c7b41ba3e6b1cd2214c359c76f9b6ff8417
nightshade78 <99998708+nightshade78@users.noreply.github.com>
2023-06-14 01:00:36 +0200

    Fixed script for pushing baseimage to Docker Hub

--- image/push-baseimage.sh
+++ image/push-baseimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source config
 docker login
-docker push nightshade78/seafile-urup-baseimage:${SEAFILE_VERSION}-${UBUNTU_VERSION}-${BASEIMAGE_BUILD}
-docker push nightshade78/seafile-urup-baseimage:${UBUNTU_VERSION}-latest
+docker push nightshade78/seafile-urup-baseimage:${SEAFILE_VERSION}-${BASEIMAGE_BUILD}
+docker push nightshade78/seafile-urup-baseimage:latest
 docker logout
