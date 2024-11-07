e1bdf8d514a63cbac2e045d6700c0ea4d54efb03
nightshade78 <99998708+nightshade78@users.noreply.github.com>
2024-11-07 13:31:07 +0100

    Updated Seafile to 11.0.13; include latest fixes from official image

--- image/push-baseimage.sh
+++ image/push-baseimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source config
-docker login
+docker login -u nightshade78
 docker push nightshade78/seafile-urup-baseimage:${SEAFILE_VERSION}-${BASEIMAGE_BUILD}
 docker push nightshade78/seafile-urup-baseimage:latest
 docker logout
